ab3c8c9282ffeb5338904804dd710d6ee06b7208
Yomi Alalade <abayomi.alalade@gmail.com>
2025-11-18 23:51:53 +0000

    cleanup

--- src/main/java/com/eagle/api/dto/CreateBankAccountRequest.java
+++ src/main/java/com/eagle/api/dto/CreateBankAccountRequest.java
@@ -1,11 +1,14 @@
 package com.eagle.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBankAccountRequest {
     @NotBlank
     private String name;
+
+    @NotNull
     private AccountType accountType;
 }
